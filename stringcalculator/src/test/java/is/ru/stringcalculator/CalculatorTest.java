39380fb00382ddc2521c2d90950bbab4c4bfd551
holmfridurge <holmfridur13@ru.is>
2015-10-17 18:04:44 +0000

    Stap 2: Add handles unknown number of arguments/numbers

--- stringcalculator/src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ stringcalculator/src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -28,5 +28,10 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+	
+	@Test
+	public void testMoreNumbers(){
+		assertEquals(10, Calculator.add("1,2,3,4"));
+	}
 
 }
