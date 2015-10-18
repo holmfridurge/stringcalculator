3aa46ae275e9570878972f3bd5a6a7f58cd8bc2e
holmfridurge <holmfridur13@ru.is>
2015-10-18 17:40:15 +0000

    Started from the beginning. Can handle multiple numbers

--- stringcalculator/src/main/java/is/ru/stringcalculator/Calculator.java
+++ stringcalculator/src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,44 +3,28 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		String delimiter = ",";
-		if(text.startsWith("//")) {
-			char temp = text.charAt(2);
-			String temp2 = String.valueOf(temp);
-			delimiter = temp2;
-		}
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(delimiter) || text.contains("\n")){
-			return sum(splitNumbers(text, delimiter));
+		else if(text.contains(",")){
+			return sum(splitNumbers(text));
 		}
 		else
-			return 1;
+			return toInt(text);
 	}
 
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
-
-	private static boolean isNumber(String numbers) {
-				//checks if the string only contains decimal numbers
-				return numbers.matches("[0-9]");
-	}
 	
-	private static String[] splitNumbers(String numbers, String delimiter){
-	    String newLineDel = "\n";
-		String newDelimiter = "["+delimiter+newLineDel+"]";
-		return numbers.split(newDelimiter);
+	private static String[] splitNumbers(String numbers){
+		return numbers.split(",");
 	}
 	 
     private static int sum(String[] numbers){
  	    int total = 0;
         for(String number : numbers){
-			//only adds the number if it's a decimal number
-			if(isNumber(number)) {
-				total += toInt(number);
-			}
+			total += toInt(number);
 		}
 		return total;
 	}
--- stringcalculator/src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ stringcalculator/src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -18,7 +18,12 @@ public class CalculatorTest {
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
-
+	
+	@Test
+	public void testOneDifferentNumber() {
+		assertEquals(2, Calculator.add("2"));
+	}
+	
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
@@ -28,32 +33,5 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
-	
-	@Test
-	public void testMoreNumbers(){
-		assertEquals(10, Calculator.add("1,2,3,4"));
-	}
-	
-	@Test
-	public void testNewLine() {
-		assertEquals(6, Calculator.add("1\n2,3"));
-	}
-	
-	@Test
-	public void testMultipleNewLines(){
-		assertEquals(5, Calculator.add("1\n1\n2,1"));
-	}
-	
-	@Test
-	public void testDifferentDelimiter(){
-		assertEquals(3, Calculator.add("//;\n1;2"));
-	}
-	
-	
-	
-	/*@Test
-	public void testIfNegative() {
-		assertEquals(0, Calculator.add("-1,2"));
-	}*/
 
 }
